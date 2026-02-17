1ace4270e8f870abbf01f8ecd6b7ac1329d9127c
Najto <ole1510@live.de>
2026-02-17 23:23:49 +0100

    Updated SplitSetup.tsx

--- components/SplitSetup.tsx
+++ components/SplitSetup.tsx
@@ -798,8 +798,8 @@ export const SplitSetup: React.FC<SplitSetupProps> = ({ splits, roster, minIlvl 
       <div
         className="grid gap-6 animate-in fade-in slide-in-from-bottom-4 duration-700 pb-20 mx-auto"
         style={{
-          gridTemplateColumns: `repeat(${currentSplits.length}, minmax(520px, 800px))`,
-          maxWidth: `${currentSplits.length * 520 + (currentSplits.length - 1) * 24}px`,
+          gridTemplateColumns: `repeat(${currentSplits.length}, minmax(420px, 520px))`,
+          maxWidth: `${currentSplits.length * 620 + (currentSplits.length - 1) * 24}px`,
         }}
       >
         {currentSplits.map((group, groupIdx) => (
